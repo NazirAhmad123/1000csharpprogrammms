a11a038fc1330f9e39607ca81b8fedbee92b48a8
NBK-14-011 <NBK-14-011@NBK-14-011>
2018-08-09 15:56:10 +0800

    Added a new program to find out if an entered number is even or odd.

---
+++ 1000csharpprogramms/Program.cs
@@ -0,0 +1,41 @@
+﻿using _1000csharpprogramms.Programms;
+using System;
+
+
+
+namespace _1000csharpprogramms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1, num2, temp;
+
+            //Console.WriteLine("\nEnter two numbers: ");
+            //num1 = int.Parse(Console.ReadLine());
+            //num2 = int.Parse(Console.ReadLine());
+
+            //temp = num1;
+            //num1 = num2;
+            //num2 = temp;
+
+            //Console.Write("After swapping: " + num1 + ", " + num2);
+            //Console.Write("Enter a string in upper-case: ");
+            //string strr = Console.ReadLine();
+
+            //Console.Write("Here is the upper-case v of it: " + ToLower(strr));
+
+            EvenOrOdd e = new EvenOrOdd();
+            Console.Write("Enter a number: ");
+            e.EvenOdd(int.Parse(Console.ReadLine()));
+
+            Console.ReadKey();
+
+        }
+        public static string ToLower(string str)
+        {
+            str = str.ToLower();
+            return str;
+        }
+    }
+}
---
+++ 1000csharpprogramms/Programms/EvenOrOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000csharpprogramms.Programms
+{
+    class EvenOrOdd
+    {
+        public void EvenOdd(int num)
+        {
+            if(num % 2 == 0)
+            {
+                Console.Write("Entered number is even.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Write("Entered number od odd.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
